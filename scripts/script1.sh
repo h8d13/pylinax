aea94ab873e56b5699abbf37f23bf0ea395d6768
HADEON <52324046+h8d13@users.noreply.github.com>
2025-05-10 15:04:36 +0200

    Update script1.sh

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,67 +1,169 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 
 # Constants
-KEYBOARD_LAYOUT="us"
-DISK="/dev/sda"
-SWAP_SIZE_GB=4
-USERNAME="artixuser"
-USER_PASSWORD="password123"
-HOSTNAME="artix-host"
-TIMEZONE="America/New_York"
+LANGCODE="en_US"         # Language code (e.g., en_US, de_DE)
+MY_INIT="openrc"         # Init system: "openrc" or "dinit"
+MY_DISK="/dev/sda"       # Disk to install to (e.g., /dev/sda)
+SWAP_SIZE=4              # Swap size in GiB
+MY_FS="btrfs"            # Filesystem: "btrfs" or "ext4"
+ENCRYPTED="y"            # Encrypt root? "y" for yes, "n" for no
+CRYPTPASS="password123"  # Encryption password (if ENCRYPTED="y")
+REGION_CITY="America/Denver"  # Timezone (e.g., America/Denver)
+MY_HOSTNAME="artix-host" # Hostname
+ROOT_PASSWORD="root123"  # Root password
 
-# Set keyboard layout
-loadkeys $KEYBOARD_LAYOUT
-echo "Starting simplified Artix Install Script..."
+# Determine keymap
+case "$LANGCODE" in
+"en_GB")
+    MY_KEYMAP="uk"
+    ;;
+"en_US")
+    MY_KEYMAP="us"
+    ;;
+*)
+    MY_KEYMAP=$(echo "$LANGCODE" | cut -c1-2)
+    ;;
+esac
+sudo loadkeys "$MY_KEYMAP"
 
-# Disk partitioning and formatting
-if [ "$WIPE_DISK" = true ]; then
-    wipefs --all --force "$DISK"
-    echo "g
-    n
-    1
+# Check boot mode
+[ ! -d /sys/firmware/efi ] && printf "Not booted in UEFI mode. Aborting..." && exit 1
 
-    +256M
-    t
-    1
-    n
+# Partition variables
+PART1="$MY_DISK"1
+PART2="$MY_DISK"2
+case "$MY_DISK" in
+*"nvme"* | *"mmcblk"*)
+    PART1="$MY_DISK"p1
+    PART2="$MY_DISK"p2
+    ;;
+esac
 
-
-
-    w
-    " | fdisk -w always -W always "$DISK"
+# Root partition
+if [ "$ENCRYPTED" = "y" ]; then
+    MY_ROOT="/dev/mapper/root"
+else
+    MY_ROOT=$PART2
 fi
 
-# Disk formatting
-mkfs.fat -F32 "${DISK}1"
-mkfs.ext4 -O fast_commit "${DISK}2"
+# Packages
+pkgs="base base-devel $MY_INIT elogind-$MY_INIT efibootmgr grub dhcpcd wpa_supplicant connman-$MY_INIT"
+[ "$MY_FS" = "btrfs" ] && pkgs="$pkgs btrfs-progs"
+[ "$ENCRYPTED" = "y" ] && pkgs="$pkgs cryptsetup cryptsetup-$MY_INIT"
 
-# Mount partitions
-mount "${DISK}2" /mnt
-mkdir -p /mnt/{boot/EFI,etc/conf.d}
-mount "${DISK}1" /mnt/boot/EFI
+# Partition disk
+printf "label: gpt\n,550M,U\n,,\n" | sfdisk "$MY_DISK"
 
-# Swap file creation
-if [ "$SWAP_SIZE_GB" -gt 0 ]; then
-    dd if=/dev/zero of=/mnt/swapfile bs=1G count="$SWAP_SIZE_GB" status=progress
-    chmod 600 /mnt/swapfile
-    mkswap /mnt/swapfile
-    swapon /mnt/swapfile
+# Format and mount partitions
+if [ "$ENCRYPTED" = "y" ]; then
+    yes "$CRYPTPASS" | cryptsetup -q luksFormat "$PART2"
+    yes "$CRYPTPASS" | cryptsetup open "$PART2" root
 fi
 
-# Generate fstab
-fstabgen -U /mnt >> /mnt/etc/fstab
+mkfs.fat -F 32 "$PART1"
 
-# Set hostname
-echo "$HOSTNAME" > /mnt/etc/hostname
+if [ "$MY_FS" = "ext4" ]; then
+    yes | mkfs.ext4 "$MY_ROOT"
+    mount "$MY_ROOT" /mnt
 
-# Install base packages
-BASE_DEVEL='db diffutils gc guile libisl libmpc perl autoconf automake bash dash binutils bison esysusers etmpfiles fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make'
-basestrap /mnt base $BASE_DEVEL openrc elogind-openrc linux linux-firmware git man-db iptables-nft
+    # Create swapfile
+    mkdir /mnt/swap
+    fallocate -l "$SWAP_SIZE"G /mnt/swap/swapfile
+    chmod 600 /mnt/swap/swapfile
+    mkswap /mnt/swap/swapfile
+elif [ "$MY_FS" = "btrfs" ]; then
+    mkfs.btrfs -f "$MY_ROOT"
 
-# Set timezone
-ln -sf "/usr/share/zoneinfo/$TIMEZONE" /mnt/etc/localtime
+    # Create subvolumes
+    mount "$MY_ROOT" /mnt
+    btrfs subvolume create /mnt/root
+    btrfs subvolume create /mnt/home
+    btrfs subvolume create /mnt/swap
+    umount -R /mnt
 
-# Set username and password
-echo "$USERNAME:$USER_PASSWORD" | chpasswd --root /mnt
+    # Mount subvolumes
+    mount -t btrfs -o compress=zstd,subvol=root "$MY_ROOT" /mnt
+    mkdir /mnt/home
+    mkdir /mnt/swap
+    mount -t btrfs -o compress=zstd,subvol=home "$MY_ROOT" /mnt/home
+    mount -t btrfs -o noatime,nodatacow,subvol=swap "$MY_ROOT" /mnt/swap
 
-echo "Simplified installation completed. Reboot to use your new system."
+    # Create swapfile
+    btrfs filesystem mkswapfile -s "$SWAP_SIZE"G /mnt/swap/swapfile
+fi
+
+swapon /mnt/swap/swapfile
+
+mkdir /mnt/boot
+mount "$PART1" /mnt/boot
+
+case $(grep vendor /proc/cpuinfo) in
+*"Intel"*)
+    pkgs="$pkgs intel-ucode"
+    ;;
+*"Amd"*)
+    pkgs="$pkgs amd-ucode"
+    ;;
+esac
+
+unset --
+IFS=" "
+for pkg in $pkgs; do
+    set -- "$@" "$pkg"
+done
+
+# Install base system and kernel
+basestrap /mnt "$@"
+basestrap /mnt linux linux-firmware linux-headers mkinitcpio
+fstabgen -U /mnt >/mnt/etc/fstab
+
+# Chroot configuration
+ln -sf /usr/share/zoneinfo/"$REGION_CITY" /etc/localtime
+hwclock --systohc
+
+# Localization
+printf "%s.UTF-8 UTF-8\n" "$LANGCODE" >>/etc/locale.gen
+locale-gen
+printf "LANG=%s.UTF-8\n" "$LANGCODE" >/etc/locale.conf
+printf "KEYMAP=%s\n" "$MY_KEYMAP" >/etc/vconsole.conf
+
+# Host setup
+printf '%s\n' "$MY_HOSTNAME" >/etc/hostname
+[ "$MY_INIT" = "openrc" ] && printf 'hostname="%s"\n' "$MY_HOSTNAME" >/etc/conf.d/hostname
+printf "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t%s.localdomain\t%s\n" "$MY_HOSTNAME" "$MY_HOSTNAME" >/etc/hosts
+
+# Install boot loader
+root_uuid=$(blkid "$PART2" -o value -s UUID)
+if [ "$ENCRYPTED" = "y" ]; then
+    my_params="cryptdevice=UUID=$root_uuid:root root=/dev/mapper/root"
+fi
+sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"$my_params\"/g" /etc/default/grub
+[ "$ENCRYPTED" = "y" ] && sed -i '/GRUB_ENABLE_CRYPTODISK=y/s/^#//g' /etc/default/grub
+
+grub-install --target=x86_64-efi --efi-directory=/boot --recheck
+grub-install --target=x86_64-efi --efi-directory=/boot --removable --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Root user
+yes "$ROOT_PASSWORD" | passwd
+
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+
+# Init system setup
+if [ "$MY_INIT" = "openrc" ]; then
+    sed -i '/rc_need="localmount"/s/^#//g' /etc/conf.d/swap
+    rc-update add connmand default
+elif [ "$MY_INIT" = "dinit" ]; then
+    ln -s /etc/dinit.d/connmand /etc/dinit.d/boot.d/
+fi
+
+# Configure mkinitcpio
+[ "$MY_FS" = "btrfs" ] && sed -i 's/BINARIES=()/BINARIES=(\/usr\/bin\/btrfs)/g' /etc/mkinitcpio.conf
+if [ "$ENCRYPTED" = "y" ]; then
+    sed -i 's/^HOOKS.*$/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)/g' /etc/mkinitcpio.conf
+else
+    sed -i 's/^HOOKS.*$/HOOKS=(base udev autodetect keyboard keymap modconf block filesystems fsck)/g' /etc/mkinitcpio.conf
+fi
+mkinitcpio -P
+
+printf '\nYou may now poweroff.\n'
